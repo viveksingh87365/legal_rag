007c1063b14696bf09af5ea20a60671ee6d51828
vivek kumar <vivekkumar@viveks-MacBook-Air.local>
2026-05-28 16:58:11 +0530

    fix indentation line 80

--- app_streamlit.py
+++ app_streamlit.py
@@ -12,8 +12,6 @@ import io
 import sys
 from contextlib import redirect_stdout
 
-# Import your script's main logic
-# Assumes scripts/ask_rag.py has a function or can be invoked
 import scripts.ask_rag as ask_rag
 
 
